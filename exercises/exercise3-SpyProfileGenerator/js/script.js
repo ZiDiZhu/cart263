649732e3959623694e03d4eb63902341ac77ec70
Nebularswamp <40129612+Nebularswamp@users.noreply.github.com>
2021-02-06 05:16:02 -0500

    E3: made it into Band members generator!

--- exercises/exercise3-SpyProfileGenerator/js/script.js
+++ exercises/exercise3-SpyProfileGenerator/js/script.js
@@ -1,25 +1,34 @@
 "use strict";
 
 /**
-Spy Profile Generator
+Band Concept Generator
 Zi Di Zhu
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+concept generator for your hypothetical, experimental music band!
 */
 
 /**
 Description of preload
 */
 
-let spyProfile = {
-  name: `**REDACTED**`,
-  alias: `**REDACTED**`,
-  secretWeapon: `**REDACTED**`,
+let bandProfile = {
+  name: ``,
+  genre: ``,
+  nbrOfMembers: ``,
 }
 
-function preload() {
+let members = []
 
+let instrumentData = undefined;
+let namesData = undefined;
+let namePrefixeData = undefined;
+let genreData = undefined;
+
+function preload() {
+  namePrefixeData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/humans/prefixes.json`);
+  namesData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/humans/wrestlers.json`);
+  instrumentData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/music/instruments.json`);
+  genreData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/music/genres.json`);
 }
 
 
@@ -29,7 +38,10 @@ Description of setup
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  spyProfile.name = prompt(`Name?`);
+  //bandProfile.name = prompt(`What's the name of your band?`);
+  bandProfile.genre = random(genreData.genres);
+  bandProfile.nbrOfMembers = random(2,4);
+  generateMember();
 }
 
 
@@ -39,12 +51,11 @@ Description of draw()
 function draw() {
   background(20);
 
-  let profile = `** CONDIDENTIAL! >:( **
+  let profile = `** ${bandProfile.name} **
+  Genre: ${bandProfile.genre}
+  Members:
+  `
 
-  Name: ${spyProfile.name}
-  Alias: ${spyProfile.alias}
-  Secret Weapon: ${spyProfile.secretWeapon}`;
-  
   push();
   textFont(`Helvetica`);
   textSize(24);
@@ -53,4 +64,41 @@ function draw() {
   text(profile,100,100);
 
   pop();
+
+  for (let i = 0; i < bandProfile.nbrOfMembers; i++){
+
+    let memberProfile = `- ${members[i].title} ${members[i].name}
+    ${members[i].instrument} player,`;
+
+    push();
+    textFont(`Helvetica`);
+    textSize(18);
+    textAlign(LEFT,TOP);
+    fill(members[i].r,members[i].g,members[i].b);
+    text(memberProfile,200,200 + 100*i);
+
+    pop();
+  }
+
+
+
+}
+
+function createMember(title, name, instrument,r,g,b){
+  let member = {
+    title : title,
+    name : name,
+    instrument : instrument,
+    r:r,
+    g:g,
+    b:b
+  };
+  return member;
+}
+
+function generateMember(){
+  for (let i = 0; i < bandProfile.nbrOfMembers; i++){
+    let member = createMember(random(namePrefixeData.prefixes), random(namesData.wrestlers), random(instrumentData.instruments),random(0,255),random(0,255),random(0,255));
+    members.push(member);
+  }
 }
