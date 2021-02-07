ccd90eec04b3d22ea61cf945a16f02e71e22ca6d
Nebularswamp <40129612+Nebularswamp@users.noreply.github.com>
2021-02-07 18:01:01 -0500

    E3: Band Generator

--- exercises/exercise3-SpyProfileGenerator/js/script.js
+++ exercises/exercise3-SpyProfileGenerator/js/script.js
@@ -24,6 +24,8 @@ let namesData = undefined;
 let namePrefixeData = undefined;
 let genreData = undefined;
 
+let button;
+
 function preload() {
   namePrefixeData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/humans/prefixes.json`);
   namesData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/humans/wrestlers.json`);
@@ -38,17 +40,21 @@ Description of setup
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  //bandProfile.name = prompt(`What's the name of your band?`);
   bandProfile.genre = random(genreData.genres);
   bandProfile.nbrOfMembers = random(2,4);
   generateMember();
+
+  button = createButton('shuffle');
+  button.position(19, 19);
+  button.mousePressed(shuffleBand);
+
+  button = createButton('Name your band');
+  button.position(119, 19);
+  button.mousePressed(renameBand);
 }
 
 
-/**
-Description of draw()
-*/
-function draw() {
+function display() {
   background(20);
 
   let profile = `** ${bandProfile.name} **
@@ -80,8 +86,20 @@ function draw() {
     pop();
   }
 
+}
 
+function renameBand(){
+  bandProfile.name = prompt(`What's the name of your band?`);
+  display();
+}
 
+function shuffleBand(){
+  while(members.length > 0) {
+    members.pop();
+}
+  bandProfile.nbrOfMembers = random(2,4);
+  generateMember();
+  bandProfile.genre = random(genreData.genres);
 }
 
 function createMember(title, name, instrument,r,g,b){
@@ -101,4 +119,5 @@ function generateMember(){
     let member = createMember(random(namePrefixeData.prefixes), random(namesData.wrestlers), random(instrumentData.instruments),random(0,255),random(0,255),random(0,255));
     members.push(member);
   }
+  display();
 }
