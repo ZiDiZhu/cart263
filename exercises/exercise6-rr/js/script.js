2d06c3b7edfe9c3977e48da8233ddb60b814e237
Nebularswamp <40129612+Nebularswamp@users.noreply.github.com>
2021-03-16 23:59:44 -0400

    E6: UnReactionist
    - click on redacted text to reveal secret
    - added sfx and vfx
    - the text is from the call of cthulhu

--- exercises/exercise6-rr/js/script.js
+++ exercises/exercise6-rr/js/script.js
@@ -1,11 +1,59 @@
-/**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
+/*
+Unravel the secret text
+Zidi
+You are trying to reveal some secrets
 */
 
 "use strict";
 
-// Code goes here
+let $secrets;
+
+var x = document.getElementById("thunder");
+var c = document.getElementById("cthulhu");
+
+setup();
+
+
+function setup() {
+  $secrets = $(`.secret`);
+  // Click to reveal redacted paragraphs
+  $secrets.on(`click`, unredact);
+  c.style.opacity = 0;
+};
+
+function unredact() {
+  $(this).removeClass(`redacted`);
+  $(this).addClass(`revealed`);
+  thunder.play();
+  fadeIn(c, 0);
+}
+
+
+function fadeIn(element, opacity) {
+  opacity += 0.01;
+  element.style[`opacity`] = opacity;
+  if (opacity < 1) {
+    requestAnimationFrame(function() {
+      fadeIn(element, opacity);
+    });
+  }
+  else {
+    fadeOut(element, 1);
+  }
+}
+
+
+function fadeOut(element, opacity) {
+  // Increase the opacity
+  opacity -= 0.01;
+  element.style[`opacity`] = opacity;
+  // Check if opacity is still less than 1
+  if (opacity >0) {
+    requestAnimationFrame(function() {
+      fadeOut(element, opacity);
+    });
+  }
+  else {
+    // Do nothing
+  }
+}
