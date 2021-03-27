51e73f36f9ab65e18d8c0c398c8aadcc61ebb70b
Nebularswamp <40129612+Nebularswamp@users.noreply.github.com>
2021-03-26 21:05:01 -0400

    P2: attempting to fix the stuck on loading issue

--- projects/project2-fruitjam-prototype/js/assetLoader.js
+++ projects/project2-fruitjam-prototype/js/assetLoader.js
@@ -12,23 +12,23 @@ function preload() {
   //sounds
 
   //drums
-  drum[0] = loadSound(`assets/sounds/808drum1.wav`);
-  drum[1] = loadSound(`assets/sounds/808drum2.wav`);
-  drum[2] = loadSound(`assets/sounds/808drum3.wav`);
+  drum[0] = loadSound('assets/sounds/808drum1.wav');
+  drum[1] = loadSound('assets/sounds/808drum2.wav');
+  drum[2] = loadSound('assets/sounds/808drum3.wav');
 
   //bass
-  bass[0] = loadSound(`assets/sounds/bass1.wav`);
-  bass[1] = loadSound(`assets/sounds/bass2.wav`);
-  bass[2] = loadSound(`assets/sounds/bass3.wav`);
+  bass[0] = loadSound('assets/sounds/bass1.wav');
+  bass[1] = loadSound('assets/sounds/bass2.wav');
+  bass[2] = loadSound('assets/sounds/bass3.wav');
 
   //piano
-  piano[0] = loadSound(`assets/sounds/softpiano1.wav`);
-  piano[1] = loadSound(`assets/sounds/softpiano2.wav`);
-  piano[2] = loadSound(`assets/sounds/softpiano3.wav`);
+  piano[0] = loadSound('assets/sounds/softpiano1.wav');
+  piano[1] = loadSound('assets/sounds/softpiano2.wav');
+  piano[2] = loadSound('assets/sounds/softpiano3.wav');
 
   //strings
-  string[0] = loadSound(`assets/sounds/string1.wav`);
-  string[1] = loadSound(`assets/sounds/string2.wav`);
-  string[2] = loadSound(`assets/sounds/string3.wav`);
+  string[0] = loadSound('assets/sounds/string1.wav');
+  string[1] = loadSound('assets/sounds/string2.wav');
+  string[2] = loadSound('assets/sounds/string3.wav');
 
 }
